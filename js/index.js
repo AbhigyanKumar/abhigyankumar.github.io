9e250de209c6104ee5b913f2e5a179c52217d0ca
Abhigyan Kumar <44155310+AbhigyanKumar@users.noreply.github.com>
2020-09-19 19:16:27 +0530

    Some bad changes

--- js/index.js
+++ js/index.js
@@ -1,9 +1,16 @@
 const autoType = new Typed(".auto-type", {
-	strings: ["developer", "researcher", "optimist", "internet guy", "learner"],
+	strings: [
+		"a developer", 
+		"a researcher", 
+		"an optimist", 
+		"an internet guy", 
+		"a learner", 
+		"a friend"
+	],
 	typeSpeed: 100,
 	backDelay: 1000,
 	smartBackspace: true,
-	backSpeed: 300,
+	backSpeed: 100,
 	showCursor: true,
 	fadeOut: false,
 	loop: true
