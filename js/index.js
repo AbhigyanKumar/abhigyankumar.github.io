138b6b125bdef054c2162ffbefcb6a76c53a03f5
Abhigyan Kumar <44155310+AbhigyanKumar@users.noreply.github.com>
2020-10-10 18:34:41 +0530

    i fu*cked this up

--- js/index.js
+++
@@ -1,1 +0,0 @@
-const autoType=new Typed(".auto-type", {strings: ["a developer","a researcher","an optimist","an internet guy","a learner","a friend","a student","a inquisitive"],typeSpeed: 100,backDelay: 1000,smartBackspace: true,backSpeed: 100,showCursor: true,fadeOut: false,loop: true});console.clear();console.warn("%cPlease do not type anything here.", "font-size: 30px; color: red;");
